0851f3916d8caf630ef8e1994d37d768452f17b7
tillua467 <tillua467@gmail.com>
2025-02-15 00:29:58 +0600

    Web: Add Home page (index.html)

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.querySelector('.menu');
 
-    // Toggle the 'active' class on the menu when the button is clicked
     menuToggle.addEventListener('click', function () {
     menu.classList.toggle('active');
     });
+
+    /* --- Screenshot Slider Functionality --- */
+    const screenshotImg = document.getElementById('screenshot');
+    // Array of screenshot
+    const screenshots = ['ss1.jpg', 'ss2.jpg', 'ss3.jpg'];
+    let currentIndex = 0;
+
+    // Function to change to the next screenshot
+    function nextScreenshot() {
+    currentIndex = (currentIndex + 1) % screenshots.length;
+    screenshotImg.src = 'images/' + screenshots[currentIndex];
+    }
+
+    // Listen for clicks on the mobile screen area to change the screenshot
+    const mobileInner = document.querySelector('.mobile-inner');
+    mobileInner.addEventListener('click', nextScreenshot);
 });
